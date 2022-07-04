4a8656d7a6bde2d92f09924463842a5d24bc531c
jenishadholariya <jenishadholariya5@gmail.com>
2022-07-04 12:24:37 +0530

    CRUD-function

--- src/Container/Appointment-form/Appoinment.js
+++ src/Container/Appointment-form/Appoinment.js
@@ -7,7 +7,9 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { NavLink } from "react-router-dom";
+import * as yup from 'yup';
 import Box from '@mui/material/Box';
+import { Formik, useFormik, Form } from 'formik';
 import { display } from '@mui/system';
 
 function Appoinment(props) {
@@ -20,6 +22,31 @@ function Appoinment(props) {
     const handleClose = () => {
         setOpen(false);
     };
+
+    let schema = yup.object().shape({
+        name: yup.string().required("Enter your name"),
+        email: yup.number().required("enter your price").positive().integer(),
+        location: yup.string().required("Enter you expiry date"),
+        contactnumber: yup.string().required("Enter your equtity"),
+        message: yup.string().required("Enter your message")
+    });
+
+    const formikobj = useFormik({
+        initialValues: {
+            name: '',
+            price: '',
+            expiry: '',
+            equtity: '',
+        },
+        validationSchema: schema,
+        onSubmit: values => {
+            console.log(values);
+            innerdata(values);
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    const [handleChange, handlesubmit, handleBlur, errors, touched] = formikobj;
     return (
         <div>
             <h1>Appoinment-Form</h1>
@@ -29,57 +56,54 @@ function Appoinment(props) {
             </Button>
             <Dialog open={open} onClose={handleClose} fullWidth>
                 <DialogTitle>Appoinment-Form</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Enter your details and submit your document.
-                    </DialogContentText>
-                    <Box fullWidth
-                        sx={{
-                            alignItems: 'center',
-                            '& > :not(style)': { m: 1 },
-                        }}
-                    >
-                        <TextField
-                            helperText="Please enter your name"
-                            id="demo-helper-text-misaligned"
-                            label="Name"
-                            fullWidth
-                        />
+                <Formik values={formikobj}>
+                    <From onSubmit={handlesubmit}>
+                        <DialogContent>
+                            <DialogContentText>
+                                Enter your details and submit your document.
+                            </DialogContentText>
+                            <Box fullWidth
+                                sx={{
+                                    alignItems: 'center',
+                                    '& > :not(style)': { m: 1 },
+                                }}
+                            >
+                                <TextField
+                                    id="demo-helper-text-misaligned"
+                                    label="name"
+                                    fullWidth
+                                />
 
-                        <TextField
-                            helperText="Please enter your email"
-                            id="demo-helper-text-misaligned"
-                            label="email"
-                            fullWidth
-                        />
+                                <TextField
+                                    id="demo-helper-text-misaligned"
+                                    label="email"
+                                    fullWidth
+                                />
 
-                        <TextField
-                            helperText="Please enter your location"
-                            id="demo-helper-text-misaligned"
-                            label="location"
-                        // fullWidth
-                        />
+                                <TextField
+                                    id="demo-helper-text-misaligned"
+                                    label="location"
+                                />
 
-                        <TextField
-                            helperText="Please enter your contact number"
-                            id="demo-helper-text-misaligned"
-                            label="contact number"
-                        // fullWidth
-                        />
+                                <TextField
+                                    id="demo-helper-text-misaligned"
+                                    label="contactnumber"
+                                />
 
-                        <TextField
-                            helperText="Please enter your message"
-                            id="demo-helper-text-misaligned"
-                            label="message"
-                            fullWidth
-                        />
+                                <TextField
+                                    id="demo-helper-text-misaligned"
+                                    label="message"
+                                    fullWidth
+                                />
 
-                    </Box>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Submit</Button>
-                </DialogActions>
+                            </Box>
+                            <DialogActions>
+                                <Button >Cancel</Button>
+                                <Button type="submit">Submit</Button>
+                            </DialogActions>
+                        </DialogContent>
+                    </From>
+                </Formik>
             </Dialog>
         </div>
     );
